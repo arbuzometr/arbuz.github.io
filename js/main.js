54b680943c19c3a39727ce8ee622b3c3d54fb85d
arbuzometr <arbuzometr@yandex.ru>
2018-08-01 15:46:51 +0300

    Changed algorithm

--- js/main.js
+++ js/main.js
@@ -1,55 +1,82 @@
 // button.onclick = countMelon;
 
-document.querySelector("#input-circumference").addEventListener("change", countMelon, false);
-document.querySelector("#input-circumference").addEventListener("keyup", countMelon, false);
+document.querySelector("#input-circumference-horizontal").addEventListener("change", countMelonByCirc, false);
+document.querySelector("#input-circumference-horizontal").addEventListener("keyup", countMelonByCirc, false);
 
-document.querySelector("#input-weight").addEventListener("change", countMelon, false);
-document.querySelector("#input-weight").addEventListener("keyup", countMelon, false);
+document.querySelector("#input-circumference-vertical").addEventListener("change", countMelonByCirc, false);
+document.querySelector("#input-circumference-vertical").addEventListener("keyup", countMelonByCirc, false);
 
-function countMelon() {
-    let circumference = document.querySelector("#input-circumference").value;
-    let weight = document.querySelector("#input-weight").value;
+document.querySelector("#input-real-weight").addEventListener("change", countMelonByCirc, false);
+document.querySelector("#input-real-weight").addEventListener("keyup", countMelonByCirc, false);
 
-    if (+circumference !== 0) {
+// document.querySelector("#input-weight").addEventListener("change", countMelonByWeight, false);
+// document.querySelector("#input-weight").addEventListener("keyup", countMelonByWeight, false);
+
+function countMelonByCirc() {
+    let cHorizontal = Number(document.querySelector("#input-circumference-horizontal").value);
+    let cVertical = Number(document.querySelector("#input-circumference-vertical").value);
+    let realWeight = Number(document.querySelector("#input-real-weight").value);
+
+
+    var circumference;
+    if (cHorizontal !== 0 && cVertical !== 0) {
+        circumference = (cHorizontal + cVertical) / 2;
+    } else if (+cHorizontal !== 0) {
+        circumference = cHorizontal;
+    }
+
+    if (cHorizontal !== 0) {
         let idealWeight = getIdealWeight(circumference);
         document.querySelector("#result-weight").value = idealWeight;
+        document.querySelector("#result-weight-max").innerHTML = 'Если реальный вес арбуза меньше <b>' + getIdealWeightMax(idealWeight) + 'кг</b>, то скорее всего он переспел';
 
-        let idealWeightMax = getIdealWeightMax(idealWeight);
-        document.querySelector("#result-weight-max").innerHTML = 'но не менее <b>' + idealWeightMax + 'г</b>*';
+        if (realWeight !== 0) {
+            document.querySelector("#ripeness-percent").innerHTML = getRipenessByCircumferencePercent(realWeight, idealWeight) + '% спелости';
+            document.querySelector("#ripeness-resume").innerHTML = getRipenessByCircumference(realWeight, idealWeight);
+        }
     }
 
-    if (+weight !== 0) {
+}
+
+function countMelonByWeight() {
+    let weight = Number(document.querySelector("#input-weight").value);
+
+    if (weight !== 0) {
         document.querySelector("#result-circ").value = getIdealCircumference(weight);
     }
-
 }
 
 //L > 4.1*∛m
 function getIdealCircumference(weight) {
-    return Math.round(Math.cbrt(weight) * 4.1);
-}
-// m=L3*0.017
-function getIdealWeight(circumference) {
-    return Math.round(Math.pow(circumference, 3) * 0.017);
+    return Math.round(Math.cbrt(weight) * 4.1 * 10);
 }
 
-function getIdealWeightMax(weight) {
-    return Math.round(weight - (weight / 100) * 7);
-}
-
-function getRipenessByCircumference(circumference, idealCircumference) {
+function getRipenessByWeight(circumference, idealCircumference) {
     return Math.round(100 + (circumference - idealCircumference) / (idealCircumference / 100));
 }
 
-function getRipenessByWeight(weight, idealweight) {
+// m=L3*0.017
+function getIdealWeight(circumference) {
+    return (Math.pow(circumference, 3) * 0.017 / 1000).toFixed(3);
+}
+
+function getIdealWeightMax(weight) {
+    return (weight * 0.93).toFixed(3);
+}
+
+function getRipenessByCircumferencePercent(realw, idealw) {
+    return Math.round(100 - (realw - idealw) / (idealw / 100));
+}
+
+function getRipenessByCircumference(weight, idealweight) {
+
     var str = '';
-    console.log(weight, idealweight * 0.9)
-    if (idealweight > weight && weight < idealweight * 0.8) {
-        str = 'ПЕРЕСПЕЛЫЙ';
-    } else if (idealweight > weight && weight > idealweight * 0.8) {
-        str = 'СПЕЛЫЙ';
+    if (idealweight > weight && weight < idealweight * 0.93) {
+        str = '<span class="result result_overripe">Переспелый</span>';
+    } else if (idealweight > weight && weight > idealweight * 0.93) {
+        str = '<span class="result result_ripe">Спелый</span>';
     } else {
-        str = '✖ НЕСПЕЛЫЙ';
+        str = '<span class="result result_notripe">Неспелый</span>';
     }
 
     return str;
