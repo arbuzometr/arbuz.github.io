87127efff352dca9b4128ca2cd3701263e559958
arbuzometr <arbuzometr@yandex.ru>
2018-08-02 15:08:25 +0300

    added calc page

--- js/main.js
+++ js/main.js
@@ -9,33 +9,31 @@ document.querySelector("#input-circumference-vertical").addEventListener("keyup"
 document.querySelector("#input-real-weight").addEventListener("change", countMelonByCirc, false);
 document.querySelector("#input-real-weight").addEventListener("keyup", countMelonByCirc, false);
 
-// document.querySelector("#input-weight").addEventListener("change", countMelonByWeight, false);
-// document.querySelector("#input-weight").addEventListener("keyup", countMelonByWeight, false);
-
 function countMelonByCirc() {
     let cHorizontal = Number(document.querySelector("#input-circumference-horizontal").value);
     let cVertical = Number(document.querySelector("#input-circumference-vertical").value);
     let realWeight = Number(document.querySelector("#input-real-weight").value);
 
-
-    var circumference;
+    /* counting average circumference */
+    var circumference = 0;
     if (cHorizontal !== 0 && cVertical !== 0) {
         circumference = (cHorizontal + cVertical) / 2;
-    } else if (+cHorizontal !== 0) {
+    } else if (cHorizontal !== 0) {
         circumference = cHorizontal;
     }
 
-    if (cHorizontal !== 0) {
+    if (circumference !== 0) {
         let idealWeight = getIdealWeight(circumference);
         document.querySelector("#result-weight").value = idealWeight;
-        document.querySelector("#result-weight-max").innerHTML = 'Если реальный вес арбуза меньше <b>' + getIdealWeightMax(idealWeight) + 'кг</b>, то скорее всего он переспел';
+        document.querySelector("#result-weight-max").innerHTML = 'Если реальный вес арбуза меньше <b>' +
+            getIdealWeightMax(idealWeight) + 'кг</b>, то скорее всего он переспел';
 
-        if (realWeight !== 0) {
-            document.querySelector("#ripeness-percent").innerHTML = getRipenessByCircumferencePercent(realWeight, idealWeight) + '% спелости';
+        let ripenessPercent = getRipenessByCircumferencePercent(realWeight, idealWeight);
+        if (realWeight !== 0 && isFinite(ripenessPercent)) {
+            document.querySelector("#ripeness-percent").innerHTML = 'Индекс спелости: ' + ripenessPercent + '%';
             document.querySelector("#ripeness-resume").innerHTML = getRipenessByCircumference(realWeight, idealWeight);
         }
     }
-
 }
 
 function countMelonByWeight() {
@@ -55,13 +53,13 @@ function getRipenessByWeight(circumference, idealCircumference) {
     return Math.round(100 + (circumference - idealCircumference) / (idealCircumference / 100));
 }
 
-// m=L3*0.017
+// m=L^3*0.017
 function getIdealWeight(circumference) {
     return (Math.pow(circumference, 3) * 0.017 / 1000).toFixed(3);
 }
 
 function getIdealWeightMax(weight) {
-    return (weight * 0.93).toFixed(3);
+    return (weight * 0.9).toFixed(3);
 }
 
 function getRipenessByCircumferencePercent(realw, idealw) {
@@ -71,12 +69,14 @@ function getRipenessByCircumferencePercent(realw, idealw) {
 function getRipenessByCircumference(weight, idealweight) {
 
     var str = '';
-    if (idealweight > weight && weight < idealweight * 0.93) {
-        str = '<span class="result result_overripe">Переспелый</span>';
-    } else if (idealweight > weight && weight > idealweight * 0.93) {
-        str = '<span class="result result_ripe">Спелый</span>';
+    if (weight < idealweight * 0.9 && weight > idealweight * 0.8) {
+        str = '<div class="result result_overripe">Скорее всего переспелый</div>';
+    } else if (weight <= idealweight * 0.8) {
+        str = '<div class="result result_overripe">Переспелый</div>';
+    } else if (weight >= idealweight * 0.9 && weight <= idealweight) {
+        str = '<div class="result result_ripe">Спелый</div>';
     } else {
-        str = '<span class="result result_notripe">Неспелый</span>';
+        str = '<div class="result result_notripe">Неспелый</div>';
     }
 
     return str;
