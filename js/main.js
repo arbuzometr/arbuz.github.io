83592d8545b912457c40a3c1c2b78fa59f92999a
arbuzometr <arbuzometr@yandex.ru>
2018-08-07 11:21:13 +0300

    Added minor fixes

--- js/main.js
+++ js/main.js
@@ -1,49 +1,69 @@
-document.querySelector('#js-circumference-horizontal').addEventListener('change', countMelonByCirc, false);
-document.querySelector('#js-circumference-horizontal').addEventListener('keyup', countMelonByCirc, false);
+addEvent('load', window, countMelonByCirc);
 
-document.querySelector('#js-circumference-vertical').addEventListener('change', countMelonByCirc, false);
-document.querySelector('#js-circumference-vertical').addEventListener('keyup', countMelonByCirc, false);
+var circHor = document.querySelector('#js-circumference-horizontal');
+addEvent('change', circHor, countMelonByCirc);
+addEvent('keyup', circHor, countMelonByCirc);
 
-document.querySelector('#js-real-weight').addEventListener('change', countMelonByCirc, false);
-document.querySelector('#js-real-weight').addEventListener('keyup', countMelonByCirc, false);
+var circVer = document.querySelector('#js-circumference-vertical');
+addEvent('change', circVer, countMelonByCirc);
+addEvent('keyup', circVer, countMelonByCirc);
+
+var w = document.querySelector('#js-mass');
+addEvent('change', w, countMelonByCirc);
+addEvent('keyup', w, countMelonByCirc);
+
+
+function addEvent(evnt, elem, func) {
+    if (elem.addEventListener) { // W3C DOM
+        elem.addEventListener(evnt, func, false);
+    } else if (elem.attachEvent) { // IE DOM
+        elem.attachEvent("on" + evnt, func);
+    } else {
+        elem[evnt] = func;
+    }
+}
 
 function countMelonByCirc() {
-    let cHorizontal = Number(document.querySelector('#js-circumference-horizontal').value);
-    let cVertical = Number(document.querySelector('#js-circumference-vertical').value);
-    let realWeight = Number(document.querySelector('#js-real-weight').value);
+    var cHorizontal = Number(document.querySelector('#js-circumference-horizontal').value);
+    var cVertical = Number(document.querySelector('#js-circumference-vertical').value);
+    var mass = Number(document.querySelector('#js-mass').value);
 
     /* count average circumference */
     var circumference = 0;
-    if (cHorizontal !== 0 && cVertical !== 0) {
+    if (cHorizontal !== 0 && isNumeric(cHorizontal) && cVertical !== 0  && isNumeric(cVertical)) {
         circumference = (cHorizontal + cVertical) / 2;
-    } else if (cHorizontal !== 0) {
+    } else if (cHorizontal !== 0 && isNumeric(cHorizontal)) {
         circumference = cHorizontal;
     }
 
     if (circumference !== 0) {
-        let idealWeight = getIdealWeight(circumference);
-        document.querySelector('#js-ideal-weight').value = idealWeight;
-        document.querySelector('#js-ideal-weight-max').innerHTML = 'Если реальный вес арбуза меньше <b>' +
-            getIdealWeightMax(idealWeight) + 'кг</b>, то скорее всего он переспел';
+        var idealMass = getIdealMass(circumference);
+        document.querySelector('#js-ideal-mass').value = idealMass;
+        document.querySelector('#js-ideal-mass-max').innerHTML = 'Если реальный вес арбуза меньше <b>' +
+            getIdealMassMax(idealMass) + 'кг</b>, то скорее всего он переспел';
 
-        let ripenessPercent = getRipenessByCircumferencePercent(realWeight, idealWeight);
-        if (realWeight !== 0 && isFinite(ripenessPercent)) {
+        var ripenessPercent = getRipenessByCircumferencePercent(mass, idealMass);
+        if (mass !== 0 && isNumeric(ripenessPercent)) {
             document.querySelector('#js-ripeness-percent').innerHTML = 'Индекс спелости: ' + ripenessPercent + '%';
             document.querySelector('#js-ripeness-range').value = ripenessPercent;
-            document.querySelector('.ripeness-visual').classList.remove('invisible');
+            document.querySelector('.visual').classList.remove('invisible');
         }
     }
 }
 
 // m=L^3*0.017
-function getIdealWeight(circumference) {
+function getIdealMass(circumference) {
     return (Math.pow(circumference, 3) * 0.017 / 1000).toFixed(3);
 }
 
-function getIdealWeightMax(weight) {
-    return (weight * 0.9).toFixed(3);
+function getIdealMassMax(m) {
+    return (m * 0.9).toFixed(3);
 }
 
-function getRipenessByCircumferencePercent(realw, idealw) {
-    return Math.round(100 - (realw - idealw) / (idealw / 100));
+function getRipenessByCircumferencePercent(m, idealm) {
+    return Math.round(100 - (m - idealm) / (idealm / 100));
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
